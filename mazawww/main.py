ef2a7b7ee00b29e28a153a7265424dc8a32829e4
Sridhar Sundaram <sridhar.sundaram@gmail.com>
2013-12-04 10:02:46 +0530

    redirect for scienceengine

--- mazawww/main.py
+++ mazawww/main.py
@@ -3,12 +3,9 @@ import webapp2
 from info import Info
 
 def main():
-    def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Hello, World!')
+  pass
 
-application = webapp2.WSGIApplication(
-                                       [
-                                         ('/info', Info),
-                                        ],
-                                       debug=True)
+application = webapp2.WSGIApplication([
+    ('/info', Info),
+    webapp2.Route('/scienceengine', webapp2.RedirectHandler, defaults={'_uri':'http://thesciencegame.mazalearn.com'}),
+    ], debug=True)
