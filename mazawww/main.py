d7dfb8c891474eb45b127b4b7eba73a2b1a944de
Sridhar Sundaram <sridhar.sundaram@gmail.com>
2013-12-04 10:13:04 +0530

    corrected redirect mistake

--- mazawww/main.py
+++ mazawww/main.py
@@ -7,5 +7,5 @@ def main():
 
 application = webapp2.WSGIApplication([
     ('/info', Info),
-    webapp2.Route('/scienceengine', webapp2.RedirectHandler, defaults={'_uri':'http://thesciencegame.mazalearn.com'}),
+    webapp2.Route('/scienceengine', webapp2.RedirectHandler, defaults={'_uri':'http://thesciencegame.mazalearn.com/scienceengine'}),
     ], debug=True)
