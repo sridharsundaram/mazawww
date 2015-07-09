2bfdfa7e81c11ff0810dc8696d40d4b47da332c5
sridhar sundaram <sridhar.sundaram@gmail.com>
2015-07-09 17:28:46 +0530

    changes for app indexing

--- mazawww/main.py
+++ mazawww/main.py
@@ -2,10 +2,15 @@
 import webapp2
 from info import Info
 
+def get_redirect_url(handler, *args, **kwargs):
+    return 'http://thesciencegame.mazalearn.com/reports/sciencejournal.jsp?topic=' + kwargs['topic'] + '&' + handler.request.query
+
 def main():
   pass
 
 application = webapp2.WSGIApplication([
     ('/info', Info),
     webapp2.Route('/scienceengine', webapp2.RedirectHandler, defaults={'_uri':'http://thesciencegame.mazalearn.com/scienceengine'}),
+    webapp2.Route('/science/<board>/<topic>', webapp2.RedirectHandler, defaults={'_uri': get_redirect_url, '_code':302}),    
     ], debug=True)
+
