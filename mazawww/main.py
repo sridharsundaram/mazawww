530005bd6feeb2c30c566253f0fa4bcf56e8232f
sridhar sundaram <sridhar.sundaram@gmail.com>
2016-03-13 12:07:24 +0530

    improvements

--- mazawww/main.py
+++ mazawww/main.py
@@ -3,7 +3,7 @@ import webapp2
 from info import Info
 
 def get_redirect_url(handler, *args, **kwargs):
-    return 'http://thesciencegame.mazalearn.com/reports/sciencejournal.jsp?topic=' + kwargs['topic'] + '&' + handler.request.query + '&tutor=D1'
+    return 'http://thesciencegame.mazalearn.com/reports/sciencejournal.jsp?topic=' + kwargs['topic'] + '&tutor=D1&' + handler.request.query
 
 def main():
   pass
@@ -12,5 +12,6 @@ application = webapp2.WSGIApplication([
     ('/info', Info),
     webapp2.Route('/scienceengine', webapp2.RedirectHandler, defaults={'_uri':'http://thesciencegame.mazalearn.com/scienceengine'}),
     webapp2.Route('/science/<board>/<topic>', webapp2.RedirectHandler, defaults={'_uri': get_redirect_url, '_code':302}),    
+    webapp2.Route('/science/<board>', webapp2.RedirectHandler, defaults={'_uri': 'http://thesciencegame.mazalearn.com/reports/sciencejournal.jsp', '_code':302}),    
     ], debug=True)
 
